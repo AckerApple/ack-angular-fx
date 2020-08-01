b8e9a5178982acd4807831ef16c8d5bcb97e32f8
Admin Apple <acker.dawn.apple@gmail.com>
2020-08-01 10:22:18 -0400

    x

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-export { AckFxModule } from "./AckFx.module"
-export { animations } from "./allFx"
+export { FxTracker } from './components/FxTracker.directive';
+export { AckFxModule } from './AckFx.module';
+export { animations } from './allFx';
